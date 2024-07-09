e3ef91360be506f963e913286df655dc15d0c27c
Labib-Ahammed <101618935+LABIB087@users.noreply.github.com>
2024-07-09 20:14:50 +0600

    Update mongodb url db.js

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURL = "mongodb+srv://labibahammed087:labibahammed@storenote.tjtqz3z.mongodb.net/?retryWrites=true&w=majority&appName=storeNote"
-
+const mongoURL = "mongodb+srv://labibahammed087:labibahammed@storenote.tjtqz3z.mongodb.net/"
 const connectToMongo = async () => {
     try {
         await mongoose.connect(mongoURL);
